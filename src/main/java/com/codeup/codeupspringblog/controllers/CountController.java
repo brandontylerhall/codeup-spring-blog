8520fe8a7c57dbc34139d3b49a6f82b771fc77fe
Brandon <bran.tyler.hall@gmail.com>
2023-02-22 11:46:51 -0500

    completed views lecture

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/CountController.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CountController {
+//    @GetMapping("/count")
+//    public String countNumbers(Model model) {
+//
+//    }
+}

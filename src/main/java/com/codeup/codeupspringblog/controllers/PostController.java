b7d47cb696d3df2da2281c3b6968cf520f861555
Brandon <bran.tyler.hall@gmail.com>
2023-02-23 11:22:55 -0500

    finished exercises. updated pom

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,30 +1,45 @@
 package com.codeup.codeupspringblog.controllers;
+
+import com.codeup.codeupspringblog.models.Ad;
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "posts index page";
+    public String showAll(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post(1, "title1", "body1");
+        Post post2 = new Post(2, "title2", "body2");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postsWithId(@PathVariable long id) {
-        return String.format("view an individual post at id %d", id);
+    public String showSpecific(@PathVariable long id, Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post(1, "title1", "body1");
+        Post post2 = new Post(2, "title2", "body2");
+        Post post3 = new Post(3, "title3", "body3");
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+        model.addAttribute("post", posts);
+        return "posts/show";
     }
+
     @GetMapping("/posts/create")
     @ResponseBody
     public String createPostForm() {
         return "view the form for creating a post";
-    }
-    @PostMapping("/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "create a new post";
     }
 }
---
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
@@ -0,0 +1,23 @@
+package com.codeup.codeupspringblog.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model model) {
+        int roll = (int) (Math.random() * 6) + 1;
+        String message = (guess == roll) ? "You guessed correctly!" : "Sorry, you are the worst.";
+        model.addAttribute("guess", guess);
+        model.addAttribute("roll", roll);
+        model.addAttribute("message", message);
+        return "roll-dice-result";
+    }
+}
